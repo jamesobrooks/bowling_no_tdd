7414f07de73b25d27d85ea8a7af1019a2ad2ee0f
James Brooks <james.brooks@jbhunt.com>
2020-08-16 21:43:22 -0500

    Pulled tenth frame out of loop to simplify. Cognitive complexity drops from 44 to 29.

--- src/score.kt
+++ src/score.kt
@@ -5,34 +5,36 @@ fun score(): Int {
     var score = 0
     val frames = mutableListOf<Frame>()
     var shortGame = false
-    for (i in 1..10) {
-        if (i != 10) {
-            var first = 0
-            if (Rolls.rolls.size > 0) first = Rolls.rolls.removeAt(0) else shortGame = true
-            if (first == 10) {
-                frames.add(Frame(i, 10, type = "strike"))
-            } else {
-                var second = 0
-                if (Rolls.rolls.size > 0) second = Rolls.rolls.removeAt(0) else shortGame = true
-                var type = ""
-                val pins = first + second
-                if (pins == 10) {
-                    type = "spare"
-                }
-                frames.add(Frame(i, first, second, type = type))
-            }
+
+    // Add first nine frames
+    for (i in 1..9) {
+        var first = 0
+        if (Rolls.rolls.size > 0) first = Rolls.rolls.removeAt(0) else shortGame = true
+        if (first == 10) {
+            frames.add(Frame(i, 10, type = "strike"))
         } else {
-            var first = 0
-            if (Rolls.rolls.size > 0) first = Rolls.rolls.removeAt(0) else shortGame = true
             var second = 0
             if (Rolls.rolls.size > 0) second = Rolls.rolls.removeAt(0) else shortGame = true
-            var third = 0
-            if (first + second >= 10) {
-                if (Rolls.rolls.size > 0) third = Rolls.rolls.removeAt(0) else shortGame = true
+            var type = ""
+            val pins = first + second
+            if (pins == 10) {
+                type = "spare"
             }
-            frames.add(Frame(10, first, second, third, type = "tenth"))
+            frames.add(Frame(i, first, second, type = type))
         }
     }
+
+    // Add tenth frame
+    var first = 0
+    if (Rolls.rolls.size > 0) first = Rolls.rolls.removeAt(0) else shortGame = true
+    var second = 0
+    if (Rolls.rolls.size > 0) second = Rolls.rolls.removeAt(0) else shortGame = true
+    var third = 0
+    if (first + second >= 10) {
+        if (Rolls.rolls.size > 0) third = Rolls.rolls.removeAt(0) else shortGame = true
+    }
+    frames.add(Frame(10, first, second, third, type = "tenth"))
+
     for (frame in frames) {
         var bonus = 0
         if (frame.type == "strike") {
