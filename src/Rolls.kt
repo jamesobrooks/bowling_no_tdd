81e991d9fed36bad60e02fdb137549d9eb4b21dd
James Brooks <james.brooks@jbhunt.com>
2020-08-16 21:56:19 -0500

    Extracted out removing next roll from Rolls.rolls into function on Rolls. This allowed removing some unnecessary code from score and changing some var's to val's. Also seems to have reduced cognitive complexity from 29 to below 15.

--- src/Rolls.kt
+++ src/Rolls.kt
@@ -1,3 +1,13 @@
+import Exceptions.TooFewRollsException
+
 object Rolls {
     val rolls = mutableListOf<Int>()
+
+    fun getNextRoll(): Int {
+        if (rolls.size == 0) {
+            rolls.clear()
+            throw TooFewRollsException("You didn't roll enough balls!")
+        }
+        return rolls.removeAt(0)
+    }
 }
--- src/score.kt
+++ src/score.kt
@@ -4,17 +4,14 @@ import Exceptions.TooManyRollsException
 fun score(): Int {
     var score = 0
     val frames = mutableListOf<Frame>()
-    var shortGame = false
 
     // Add first nine frames
     for (i in 1..9) {
-        var first = 0
-        if (Rolls.rolls.size > 0) first = Rolls.rolls.removeAt(0) else shortGame = true
+        val first = Rolls.getNextRoll()
         if (first == 10) {
             frames.add(Frame(i, 10, type = "strike"))
         } else {
-            var second = 0
-            if (Rolls.rolls.size > 0) second = Rolls.rolls.removeAt(0) else shortGame = true
+            val second = Rolls.getNextRoll()
             var type = ""
             val pins = first + second
             if (pins == 10) {
@@ -25,13 +22,11 @@ fun score(): Int {
     }
 
     // Add tenth frame
-    var first = 0
-    if (Rolls.rolls.size > 0) first = Rolls.rolls.removeAt(0) else shortGame = true
-    var second = 0
-    if (Rolls.rolls.size > 0) second = Rolls.rolls.removeAt(0) else shortGame = true
+    val first = Rolls.getNextRoll()
+    val second = Rolls.getNextRoll()
     var third = 0
     if (first + second >= 10) {
-        if (Rolls.rolls.size > 0) third = Rolls.rolls.removeAt(0) else shortGame = true
+        third = Rolls.getNextRoll()
     }
     frames.add(Frame(10, first, second, third, type = "tenth"))
 
@@ -47,9 +42,6 @@ fun score(): Int {
     if (Rolls.rolls.size > 0) {
         Rolls.rolls.clear()
         throw TooManyRollsException("Hey Kingpin! You rolled too many balls!")
-    } else if (shortGame) {
-        Rolls.rolls.clear()
-        throw TooFewRollsException("You didn't roll enough balls!")
     } else {
         Rolls.rolls.clear()
     }
